2fe803ede2b43b14f68eca3f626bf0d1cd3ef515
Ashikur Rahman Hridoy <ashikur6053@gmail.com>
2025-04-03 20:42:14 +0600

    banner part responsive done

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -6,21 +6,27 @@ import bannerImg from "../assets/images/bannerImg.png";
 const Banner = () => {
   return (
     <div>
-      <section className="py-[32px] relative z-50">
+      <section className="xl:py-[32px] relative z-50 px-2 myXL:px-0 ">
         <Container>
-          <Flex className="items-center">
-            <div className="max-w-[785px]">
-              <h1 className="leading-[7.26rem] tracking-[0.18rem] font-mon text-[96px] font-black text-[#FFFFFF] banner-heading">
+          <Flex className="items-center flex-col lg:flex-row gap-5">
+            <div className="max-w-[785px] order-1 lg:order-0">
+              <h1 className="leading-[121%] tracking-[0.18rem] font-mon text-4xl md:text-7xl lg:text-5xl text-center lg:text-left xl:text-8xl font-black text-[#FFFFFF] banner-heading">
                 Best NFTs Marketplace.
               </h1>
-              <p className="text-secondtext leading-[2.17rem] tracking-[-0.024rem] max-w-[609px] mt-[32px] mb-[60px] text-[19px] font-mon font-normal">
-                Ultrices eget pretium sit euismod mi id. In commodo auctor eget
+              <p className="text-secondtext leading-[2.17rem] tracking-[-0.024rem] max-w-[765px] mt-[32px] xl:mb-[60px] text-[19px] font-mon font-normal text-center lg:text-left">
+                Ultrices eget pretium sit euixlod mi id. In commodo auctor eget
                 congue sit. Risus, aliquam odio posuere ac in in nisl sed augue.
                 Porta aenean egestas malesuada in pulvinar enim viverra.
               </p>
-              <Flex className="py-[38px] gap-[17px]">
-                <Button text={"Get Started"} />
-                <Button text={"Create NFTs"} />
+              <Flex className="py-[38px] gap-[17px] flex-col lg:flex-row">
+                <Button
+                  text={"Get Started"}
+                  className="text-center w-[80%] mx-auto xl:w-auto xl:mx-0"
+                />
+                <Button
+                  text={"Create NFTs"}
+                  className="text-center w-[80%] mx-auto xl:w-auto xl:mx-0"
+                />
               </Flex>
             </div>
             <Flex className=" rounded-full max-w-[750px] max-h-[750px] bg-[#869BDF] overflow-hidden border-4 border-[#16142C] relative">
