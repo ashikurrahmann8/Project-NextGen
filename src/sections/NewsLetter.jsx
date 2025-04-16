148c56a14bce8d8ffb5d43441803f941167ca9bc
Ashikur Rahman Hridoy <ashikur6053@gmail.com>
2025-04-16 20:58:55 +0600

    NewsLetter Part responsive done

--- src/sections/NewsLetter.jsx
+++ src/sections/NewsLetter.jsx
@@ -2,22 +2,25 @@ import Container from "../components/Container";
 import Flex from "../components/Flex";
 import newsImg from "../assets/images/newsImg.png";
 import Button from "../components/Button.jsx";
-import Pattern from "../assets/images/Pattern.png"
+import Pattern from "../assets/images/Pattern.png";
 
 const NewsLetter = () => {
   return (
-    <section className="mt-[200px] relative ">
-      <img src={Pattern} className="absolute bottom-[-312px] left-0 max-w-[528px] max-h-[490px]" />
+    <section className="mt-[83px] lg:mt-[200px] relative ">
+      <img
+        src={Pattern}
+        className=" absolute bottom-[-312px] left-0 max-w-[528px] max-h-[490px]"
+      />
       <Container>
-        <Flex className="justify-between items-center">
+        <Flex className="justify-between items-center flex-col lg:flex-row">
           <div className="z-50">
             <img src={newsImg} />
           </div>
-          <div className="max-w-[680px]">
-            <h2 className="font-black text-[56px]">
+          <div className="xl:max-w-[680px] lg:max-w-[50%] lg:ml-[94px] xl:ml-0">
+            <h2 className="font-black text-center lg:text-left text-[32px] xl:text-[56px]">
               Subscribe And get Latest news update about NFTs.
             </h2>
-            <p className="font-normal text-lg">
+            <p className="font-normal text-lg text-center lg:text-left">
               In commodo auctor eget congue sit. Ultrices eget pretium sit
               euismod mi id. Risus, aliquam odio posuere ac in in nisl sed
               augue. Porta aenean egestas malesuada in pulvinar enim viverra.
@@ -30,7 +33,7 @@ const NewsLetter = () => {
               />
               <Button
                 text={"Subscribe"}
-                className="absolute right-[14px] top-[70px]"
+                className="absolute right-[14px]  top-[70px] translate-y-[98px] sm:translate-y-0 sm:left-auto sm:translate-x-0 left-1/2  -translate-x-1/2 flex justify-center items-center"
               />
             </form>
           </div>
