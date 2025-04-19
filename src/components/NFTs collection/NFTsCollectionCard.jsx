6de9606bd98d1d3e855440d6d16b79dfb8f753cc
Ashikur Rahman Hridoy <ashikur6053@gmail.com>
2025-04-19 11:21:42 +0600

    multiple images added to NFTsCollection Card

--- src/components/NFTs collection/NFTsCollectionCard.jsx
+++ src/components/NFTs collection/NFTsCollectionCard.jsx
@@ -1,9 +1,9 @@
-import nft1 from "../../assets/images/nftCollection2.png"
 
-const NFTsCollectionCard = () => {
+
+const NFTsCollectionCard = ({nftC}) => {
   return (
     <div className="max-w-[400px] rounded-[45%] bg-[#16192A] text-center pb-[87px]  border border-[#2E3150] mt-[60px] pt-[35px] px-[35px]">
-      <img src={nft1} className="max-w-[330px] max-h-[330px] mb-[40px] " />
+      <img src={nftC} className="max-w-[330px] max-h-[330px] mb-[40px] " />
       <h2 className="font-extrabold text-[22px] mb-[14px]">
         Broken collection
       </h2>
--- src/components/NFTs collection/NFTsCollectionContainer.jsx
+++ src/components/NFTs collection/NFTsCollectionContainer.jsx
@@ -1,13 +1,17 @@
 import Flex from "../Flex";
 import NFTsCollectionCard from "./NFTsCollectionCard";
+import nftC1 from "../../assets/images/nftC1.png";
+import nftC2 from "../../assets/images/nftC2.png";
+import nftC3 from "../../assets/images/nftC3.png";
+import nftC4 from "../../assets/images/nftC4.png";
 
 const NFTsCollectionContainer = () => {
   return (
     <Flex className="gap-x-[20px]">
-      <NFTsCollectionCard />
-      <NFTsCollectionCard />
-      <NFTsCollectionCard />
-      <NFTsCollectionCard />
+      <NFTsCollectionCard nftC={nftC1} />
+      <NFTsCollectionCard nftC={nftC2} />
+      <NFTsCollectionCard nftC={nftC3} />
+      <NFTsCollectionCard nftC={nftC4} />
     </Flex>
   );
 };
