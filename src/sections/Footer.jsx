795dde6ab6bc3aceef8c71bac5a14c0a88030e60
Ashikur Rahman Hridoy <ashikur6053@gmail.com>
2025-04-21 16:06:04 +0600

    Footer part responsive done

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,6 +1,6 @@
 import Container from "../components/Container";
 import Flex from "../components/Flex";
-import logo from "../assets/images/logo.png";
+import logo from "../assets/images/footerLogo.png"
 import SocialMedia from "../components/SocialMedia";
 import shade3 from "../assets/images/Shade 3.png";
 
@@ -43,25 +43,28 @@ let fotterContents = [
 ];
 const Footer = () => {
   return (
-    <footer className="mt-[400px] relative">
+    <footer className="mt-[90px] xl:mt-[200px] relative">
       <img
         src={shade3}
         className="absolute z-[-50] right-[0px] top-[-816px] "
       />
       <Container>
-        <Flex className="justify-between">
-          <div className="w-[400px]">
-            <img src={logo} className="mb-[25px]" />
-            <p className="font-normal text-lg mb-[25px]">
+        <Flex className="flex-col xl:flex-row xl:items-start">
+          <div className="sm:w-[100%] xl:w-1/3 sm:mb-3 w-full max-w-auto flex flex-col items-center text-center xl:items-start xl:text-left ">
+            <img src={logo} className="mb-[25px] mx-auto xl:mx-0" />
+            <p className="w-[350px] font-normal text-lg mb-[25px]">
               Ultrices eget pretium sit euismod mi id posuere ac in in nisl sed
               augue. Posuere ac in in nisl sed augue.
             </p>
             <SocialMedia />
           </div>
-          <Flex className="justify-evenly gap-[150px]">
+          <Flex className="mt-[38px] xl:w-2/3  flex-wrap justify-between gap-y-10 gap-x-6  sm:justify-evenly  px-5">
             {fotterContents.map((item) => (
               <div>
-                <h2 key={item.heading} className="font-black text-[22px] mb-[24px]">
+                <h2
+                  key={item.heading}
+                  className="font-black text-[22px] mb-[24px]"
+                >
                   {item.heading}
                 </h2>
                 <ul>
@@ -80,7 +83,7 @@ const Footer = () => {
         </Flex>
       </Container>
       {/* footer bottom part */}
-      <div className="mt-[99px] mb-[30px] border-t-2 border-[#1C1D32] "></div>
+      <div className="mt-[30px] mb-[30px] border-t-2 border-[#1C1D32] "></div>
       <Container>
         <h4 className="text-[19px] text-center mb-[30px]">
           © 2022 Copyright{" "}
